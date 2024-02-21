415ae54c05feaeb2a95755a1015ce81f14def306
Maximiliano Pizarro <maximiliano.pizarro.5@gmail.com>
2024-02-21 23:59:58 +0000

    h2Disk

--- app/src/main/webapp/app/core/jhi-navbar/jhi-navbar.vue
+++ app/src/main/webapp/app/core/jhi-navbar/jhi-navbar.vue
@@ -57,6 +57,10 @@
             <font-awesome-icon icon="cogs" />
             <span v-text="$t('global.menu.admin.configuration')">Configuration</span>
           </b-dropdown-item>
+          <b-dropdown-item v-if="!inProduction" href="./h2-console/" target="_tab">
+            <font-awesome-icon icon="database" />
+            <span v-text="$t('global.menu.admin.database')">Database</span>
+          </b-dropdown-item>
         </b-nav-item-dropdown>
         <b-nav-item-dropdown id="languagesnavBarDropdown" right v-if="languages && Object.keys(languages).length > 1">
           <span slot="button-content">
